e90d12d66dcdf929b97fafa70df11d8124ce6855
chenmingyang <1372656902@qq.com>
2023-03-17 18:28:51 +0800

    kafka

--- src/main/java/com/example/MainApplication.java
+++ src/main/java/com/example/MainApplication.java
@@ -1,11 +1,15 @@
 package com.example;
 
+import com.example.properties.KafkaProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+        ConfigurableApplicationContext application = SpringApplication.run(MainApplication.class, args);
+        KafkaProperties properties = application.getBean(KafkaProperties.class);
+        System.out.println(properties);
     }
 }
